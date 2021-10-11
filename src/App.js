016d10e21a243d353d8160ec2f224c1952a9ead1
Ayush Verma <78358911+Ayush76-crypto@users.noreply.github.com>
2021-10-11 19:04:39 +0530

    Navbar built for clone - feature

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 import Row from "./components/Row";
 import requests from "./requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="app">
-      {/* Navbar */}
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/components/Nav.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { useState } from "react";
+import { useEffect } from "react";
+import "./Nav.css"
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () =>{
+            if (window.scrollY > 100 ) {
+                handleShow(true);
+            } else handleShow(false);
+        });
+        return () => {
+            window.removeEventListener("scroll");
+        };
+    }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
