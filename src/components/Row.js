8d8b710750dcf402a61a875c4872f5eebba02f17
Ayush Verma <78358911+Ayush76-crypto@users.noreply.github.com>
2021-10-11 19:38:29 +0530

    Feat: Trailer functionality added to the clone

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const baseurl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   //React provides a snippet of code to run a statement when a condition runs
 
   useEffect(() => {
-    
     // If [], then run once else if ![] run as movie changes.
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -21,7 +23,29 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
-//   console.log(movies);
+  //   console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    }
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.name || "")
+      .then((url) => {
+        // https://www.youtube.com/watch?v=XMThy8QKqU
+        const urlParams = new URLSearchParams(new URL(url).search)
+        setTrailerUrl(urlParams.get('v'));
+      }).catch(error => console.error(error))
+    }
+  };
 
   return (
     <div className="row">
@@ -31,6 +55,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${baseurl}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -39,7 +64,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           />
         ))}
       </div>
-      {/* Container */}
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} /> }
     </div>
   );
 }
